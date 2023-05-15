908bedcf11fb5304705823ee82d709040fb306d2
Prakeerti Misra <pmi@leapwork.com>
2023-05-15 15:08:35 +0530

    adding user controls

---
+++ View/UserControls/NoteDisplay.xaml.cs
@@ -0,0 +1,53 @@
+﻿using Evernote_Clone.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Evernote_Clone.View.UserControls
+{
+    /// <summary>
+    /// Interaction logic for NoteDisplay.xaml
+    /// </summary>
+    public partial class NoteDisplay : UserControl
+    {
+
+
+        public Note Note
+        {
+            get { return (Note)GetValue(NoteProperty); }
+            set { SetValue(NoteProperty, value); }
+        }
+
+        // Using a DependencyProperty as the backing store for MyProperty.  This enables animation, styling, binding, etc...
+        public static readonly DependencyProperty NoteProperty =
+            DependencyProperty.Register("Note", typeof(Note), typeof(NoteDisplay), new PropertyMetadata(null, SetValues));
+
+        private static void SetValues(DependencyObject d, DependencyPropertyChangedEventArgs e)
+        {
+            NoteDisplay noteUserControl = d as NoteDisplay;
+
+            if (noteUserControl != null)
+            {
+                noteUserControl.DataContext = noteUserControl.Note;
+                //the data context for this user control will the NOTEBOOK property 
+                //essentially we are updating the data context for these user control evry single time the notebook property changes 
+            }
+
+        }
+        public NoteDisplay()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ View/UserControls/NotebookDisplay.xaml.cs
@@ -0,0 +1,59 @@
+﻿using Evernote_Clone.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Evernote_Clone.View.UserControls
+{
+    /// <summary>
+    /// Interaction logic for NotebookDisplay.xaml
+    /// </summary>
+    public partial class NotebookDisplay : UserControl
+    {
+        //any display notebook user control will need a notebook that can be used to bound 
+
+
+        public Notebook Notebook
+        {
+            get { return (Notebook)GetValue(NotebookProperty); }
+            set { SetValue(NotebookProperty, value); }
+        }
+
+        // Using a DependencyProperty as the backing store for MyProperty.  This enables animation, styling, binding, etc...
+        public static readonly DependencyProperty NotebookProperty =
+            DependencyProperty.Register("Notebook", typeof(Notebook), typeof(NotebookDisplay), new PropertyMetadata(null, SetValues));
+
+        private static void SetValues(DependencyObject d, DependencyPropertyChangedEventArgs e)
+        {
+            NotebookDisplay notebookUserControl= d as NotebookDisplay; 
+            if(notebookUserControl != null)
+            {
+                notebookUserControl.DataContext = notebookUserControl.Notebook;
+                //the data context for this user control will the NOTEBOOK property 
+                //essentially we are updating the data context for these user control evry single time the notebook property changes 
+            }
+
+        }
+
+        public NotebookDisplay()
+        {
+            InitializeComponent();
+        }
+
+        public static implicit operator NotebookDisplay?(NoteDisplay? v)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
