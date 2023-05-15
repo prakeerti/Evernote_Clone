e7f7e5bbda17c4d64b1a28480c6284e83cd755da
Prakeerti Misra <pmi@leapwork.com>
2023-05-15 16:54:05 +0530

    adding the rename functionality using event handlers

---
+++ ViewModel/Commands/RenameCommand.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace Evernote_Clone.ViewModel.Commands
+{
+    public class RenameCommand : ICommand
+    {
+        public NotesVM VM { get; set; }
+        public event EventHandler? CanExecuteChanged;
+
+        //we have to create a constructor to recieve the instance of notesVM 
+        public RenameCommand(NotesVM vm)
+        {
+            VM = vm;
+        }
+
+        public bool CanExecute(object? parameter)
+        {
+            return true;
+        }
+
+        public void Execute(object? parameter)
+        {
+            VM.StartEditing();
+        }
+    }
+}
--- ViewModel/NotesVM.cs
+++ ViewModel/NotesVM.cs
@@ -6,8 +6,10 @@ using System.Collections.Generic;
 using System.Collections.ObjectModel;
 using System.ComponentModel;
 using System.Linq;
+using System.Security.Cryptography.X509Certificates;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows;
 
 namespace Evernote_Clone.ViewModel
 {
@@ -18,6 +20,10 @@ namespace Evernote_Clone.ViewModel
         public ObservableCollection<Note> Notes { get; set; }
         public NewNotebookCommand NewNotebookCommand { get; set; }
         public NewNoteCommand NewNoteCommand { get; set; }
+
+        public RenameCommand RenameCommand { get; set; }
+
+        public EndEditingCommand EndEditingCommand { get; set; }
 
         private Notebook selectedNotebook;
 
@@ -38,10 +44,16 @@ namespace Evernote_Clone.ViewModel
         {
 			NewNotebookCommand = new NewNotebookCommand(this);
 			NewNoteCommand = new NewNoteCommand(this);
+			RenameCommand= new RenameCommand(this);
+			EndEditingCommand = new EndEditingCommand(this);
+			
 
 			Notebooks= new ObservableCollection<Notebook>();
 			Notes= new ObservableCollection<Note>();
 			//do do this in constructor because we donot want to initialise a new collection every time only once when this constructor runs 
+
+			//setting visibility of the rename menu item 
+			IsVisible= Visibility.Collapsed; //initially the text box will be collapsed 
 
 			GetNotebooks(); //bcoz we want the view to show the list of notebooks when the view is called immediately 
         }
@@ -108,5 +120,36 @@ namespace Evernote_Clone.ViewModel
 			PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
 		}
 
+
+		//we will prop full this visibility property as this will trigger onpropertychange event everty time there is a change in selection of the rename command 
+
+		private Visibility isVisible;
+
+		public Visibility IsVisible
+		{
+			get { return isVisible; }
+			set 
+			{ 
+				isVisible = value;
+				OnPropertyChanged("IsVisible");
+			}
+		}
+
+		public void StartEditing()
+		{
+			//when a menu item is clicked a property change will become true, which should display the text box 
+			//as is the text box remains collapsed but the moment someone click on the rename property it should be visible 
+			//we have to define one more property "visibility" to handl the visibility of the text box 
+			IsVisible = Visibility.Visible;
+			//now we just have to bind it to the text box we want to show this property 
+		}
+		//this stop editing takes a parameter notebook which should be present to stop the editing function
+		public void StopEditing(Notebook notebook)
+		{
+			IsVisible = Visibility.Collapsed;
+			DatabaseHelper.Update(notebook); //to update in db the changed name of the notebook 
+			//once the stop editing is done it should again display the collection of lists of notebooks 
+			GetNotebooks();
+		}
     }
 }
